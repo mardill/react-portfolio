9ebcc69bbb6c66106104d413ef4cc68c3eeef5b9
Mary Dillon <mary.dillon09@gmail.com>
2022-08-25 17:58:39 -0700

    adds proficiencies section

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,19 +3,34 @@ import React from 'react';
 
 
 function Resume() {
-  return (
-    <section id="work">
+    return (
+        <section id="work">
 
 
-    <div class="intro">
-        <div class="intro-wrapper">
-            <h2>My Recent Projects</h2>
-            <p>Take a look at the latest projects I've done! I've built web applicaitons
-                using HTML and CSS.
-        </p>
-    </div>
-    </div>
-    </section>
+            <div class="intro">
+                <div class="intro-wrapper">
+                    <h4>Front-end Proficiencies</h4>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>React</li>
+                        <li>Javascript</li>
+                        <li>Jquery</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                    <h4>Back-end Proficiencies</h4>
+                    <ul>
+                        <li>APIs</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>MySql, Sequelize</li>
+                        <li>Mongo, Mongoose</li>
+                        <li>Rest</li>
+                        <li>GraphQL</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     );
 }
 
