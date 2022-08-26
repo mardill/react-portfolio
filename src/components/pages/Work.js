6636c40704b1ee1284919d9223142169c18e0de9
Mary Dillon <mary.dillon09@gmail.com>
2022-08-25 17:53:19 -0700

    adds images for work samples

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -22,42 +22,42 @@ function Work() {
         <a href="https://simplicity-art-gallery.herokuapp.com/"><h3>Art Gallery</h3></a>
             <a href="https://github.com/mardill/Art-gallery" target="_blank">
                 <img src="images/artgallery.png" alt="Art Gallery"/>
-                <h3>Github Repo</h3>
+                <h5>Github Repo</h5>
             </a>
         </div>
         <div class="p2">
         <a href="https://stormy-wildwood-40521.herokuapp.com/"><h3>Text Editor</h3></a>
             <a href="https://github.com/mardill/text-editor" target="_blank">
                 <img src="images/jate.png" alt="Screenshot of Text Editor"/>
-                <h3>Github Repo</h3>
+                <h5>Github Repo</h5>
             </a>
         </div>
         <div class="p3">
         <a href="https://genrosales11.github.io/Yummy-app/"><h3>Yummy App</h3></a>
             <a href="https://github.com/mardill/Yummy-app" target="_blank">
                 <img src="images/yummy.png" alt="Yummy App"/>
-                <h3>Github Repo</h3>
+                <h5>Github Repo</h5>
             </a>
         </div>
         <div class="p4">
         <a href="https://mvc-tech-blog-mdillon.herokuapp.com/"><h3>Tech Blog</h3></a>
             <a href="https://github.com/mardill/tech-blog" target="_blank">
                 <img src="images/techblog.png" alt="Tech Blog"/>
-                <h3>Github Repo</h3>
+                <h5>Github Repo</h5>
             </a>
         </div>
         <div class="p5">
         <a href="https://mardill.github.io/weather-dashboard/"><h3>Weather Dashboard</h3></a>
             <a href="https://github.com/mardill/weather-dashboard" target="_blank">
                 <img src="images/weather.png" alt="Weather Dashboard"/>
-                <h3>Github Repo</h3>
+                <h5>Github Repo</h5>
             </a>
         </div>
         <div class="p6">
         <a href="https://mardill.github.io/mary-dillon-portfolio/"><h3>Simple CSS Portfolio</h3></a>
             <a href="https://github.com/mardill/mary-dillon-portfolio" target="_blank">
                 <img src="images/portfolio.png" alt="Simple CSS Portfolio"/>
-                <h3>Github Repo</h3>
+                <h5>Github Repo</h5>
             </a>
         </div>
     </div>
