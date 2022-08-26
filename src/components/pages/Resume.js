ca4f641167bcea1a15e5c0122e590ac03efd71ee
Mary Dillon <mary.dillon09@gmail.com>
2022-08-25 17:54:57 -0700

    adds resume page

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+function Resume() {
+  return (
+    <section id="work">
+
+
+    <div class="intro">
+        <div class="intro-wrapper">
+            <h2>My Recent Projects</h2>
+            <p>Take a look at the latest projects I've done! I've built web applicaitons
+                using HTML and CSS.
+        </p>
+    </div>
+    </div>
+    </section>
+    );
+}
+
+
+export default Resume;
